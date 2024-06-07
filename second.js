c45ab00eee42b7a4ffe5b59cb95c1fa24e8b9dd1
Sarthak Srivastava <129677475+akasar17@users.noreply.github.com>
2024-06-07 12:02:25 +0530

    Add files via upload

---
+++ second.js
@@ -0,0 +1,7 @@
+const myButton = document.getElementById("myButton"); // get a reference to the button element
+
+myButton.addEventListener("click", function() {
+  // code to execute when the button is clicked
+  console.log("Button clicked!");
+});
+
